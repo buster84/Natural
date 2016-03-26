9323dcf16037d3db03bb6d91f1ef5d39c220fd77
Yasuki Okumura <yasuki111@gmail.com>
2016-03-26 19:53:45 +0900

    Modified Generic Type name to be clear meaning

--- build.sbt
+++ build.sbt
@@ -2,7 +2,7 @@ name := """Natural"""
 
 version := "1.0"
 
-scalaVersion := "2.11.6"
+scalaVersion := "2.11.7"
 
 // Change this to another test framework if you prefer
 libraryDependencies += "org.scalatest" %% "scalatest" % "2.2.4" % "test"
@@ -12,3 +12,16 @@ libraryDependencies += "org.scala-lang" % "scala-reflect" % "2.11.6"
 // Uncomment to use Akka
 //libraryDependencies += "com.typesafe.akka" %% "akka-actor" % "2.3.11"
 
+initialCommands in console := """
+import com.github.buster84._
+type _0 = 零
+type _1 = 後者[_0]
+type _2 = 後者[_1]
+type _3 = 後者[_2]
+type _4 = 後者[_3]
+type _5 = 後者[_4]
+type _6 = 後者[_5]
+type _7 = 後者[_6]
+type _8 = 後者[_7]
+type _9 = 後者[_8]
+"""
--- src/main/scala/com/github/buster84/自然数.scala
+++ src/main/scala/com/github/buster84/自然数.scala
@@ -1,8 +1,4 @@
 package com.github.buster84
-import scala.language.experimental.macros
-import scala.language.dynamics
-import scala.reflect.macros.whitebox.Context
-import scala.annotation.tailrec
 
 sealed trait 自然数 {
   type 足す[N <: 自然数] <: 自然数
@@ -14,31 +10,4 @@ trait 零 extends 自然数 {
 
 trait 後者[N<:自然数] extends 自然数 {
   override type 足す[O <: 自然数] = 後者[N#足す[O]]
-}
-
-object Nat {
-  def toNat(i: Int): 自然数 = macro materializeWidened
-
-  def materializeWidened(c: Context)(i: c.Expr[Int]) = {
-    import c.universe._
-    i match {
-      case Literal(Constant(n: Int)) => { 
-        def mkNatTpt(i: Int): Tree = {
-          val succSym = typeOf[後者[_]].typeConstructor.typeSymbol
-          val _0Sym = typeOf[零].typeSymbol
-
-          @tailrec
-          def loop(i: Int, acc: Tree): Tree = {
-            if(i == 0) acc
-            else loop(i-1, AppliedTypeTree(Ident(succSym), List(acc)))
-          }
-
-          loop(i, Ident(_0Sym))
-        }
-        q""" new ${mkNatTpt(n)} """
-      }
-      case _ =>
-        c.abort(c.enclosingPosition, s"Expression $i does not evaluate to a non-negative Int literal")
-    }
-  }
 }
